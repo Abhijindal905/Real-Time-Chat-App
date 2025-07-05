730966963a46acc1a05a6ee5ee0dd348f91cbcd2
Abhijindal905 <abhijindal905@gmail.com>
2025-07-05 16:05:26 +0530

    added login api and tokens

--- backend/chat/core/urls.py
+++ backend/chat/core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('register/', register, name="register")
+    path('register/', register, name="register"),
+    path('login/', login, name="login"),
+    path('token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('token/refresh', TokenRefreshView.as_view(), name="token_refresh")
 ]
