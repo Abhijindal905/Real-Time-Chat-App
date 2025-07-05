7d91f06b3affdca38322a73c4bff708081a74c89
shahanshah <97621075+shazzsheikh@users.noreply.github.com>
2025-07-05 17:39:20 +0530

    register page done

--- frontend/chat/src/pages/Register.jsx
+++ frontend/chat/src/pages/Register.jsx
@@ -1,19 +1,21 @@
 function Reg(params) {
   return (
-    <div className="container grid grid-cols-6 gap-2 mx-auto my-auto shadow-lg p-4 rounded w-auto md:max-h-3/4">
-      <div className="md:col-span-2 col-span-6 bg-[#40b299]">
-        <h1 className="text-4xl text-white font-bold">Welcome Back!</h1>
-        <p className="text-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, ea.
-        </p>
-        <button className="bg-white text-[#40b299] px-6 py-2 rounded-full hover:bg-gray-200 transition duration-300">
-          Sign In
-        </button>
+    <div className="container grid grid-cols-6 gap-2 shadow-lg md:p-2 rounded-lg md:w-auto md:h-[600px]">
+      <div className="md:col-span-2 col-span-6 bg-[#40b299] rounded-lg">
+        <div className="h-full w-full mx-auto my-auto text-center flex flex-col items-center justify-center gap-4 p-8">
+          <h1 className="text-4xl text-white font-bold">Welcome Back!</h1>
+          <p className="text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, ea.
+          </p>
+          <button className="bg-white text-[#40b299] px-6 py-2 rounded-full hover:bg-gray-200 transition duration-300 cursor-pointer">
+            Sign In
+          </button>
+        </div>
       </div>
-      <div className="md:col-span-4 col-span-6">
+      <div className="md:col-span-4 col-span-6 flex flex-col items-center justify-center p-8 space-y-4">
         <h2 className="text-[#40b299] text-3xl font-bold">Create an Account</h2>
         <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</span>
-        <form className="flex flex-col gap-4 mt-4">
+        <form className="flex flex-col gap-4 mt-4 md:w-[400px] h-auto w-full">
           <input
             type="text"
             placeholder="Username"
@@ -29,7 +31,7 @@ function Reg(params) {
             placeholder="Password"
             className="border border-gray-300 p-2 rounded-md"
           />
-          <button className="bg-[#40b299] text-white px-6 py-2 rounded-full hover:bg-[#369f85] transition duration-300">
+          <button className="bg-[#40b299] text-white px-6 py-2 rounded-full hover:bg-[#369f85] transition duration-300 cursor-pointer mx-auto">
             Register
           </button>
         </form>
