85ea239bc35c2764a08f572a1435b075904483be
Abhijindal905 <abhijindal905@gmail.com>
2025-07-05 13:25:54 +0530

    added register logic

--- backend/chat/core/views.py
+++ backend/chat/core/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+@api_view(['POST'])
+def register(request):
+    username = request.data.get('username')
+    email = request.data.get('email')
+    password = request.data.get('password')
+
+
+    print("getting data", username)
+
+    if not username or not password or not email:
+        return Response({'error' : 'All fields are required'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if User.objects.filter(username = username).exists():
+        return Response({'error': 'Already exists username'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    user = User.objects.create_user(username=username, email=email, password=password)
+    return Response({'message' : 'Registered Successfully'}, status=status.HTTP_201_CREATED)
+
+
