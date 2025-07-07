64c7cfdcd7e87d347166b9dcf1a40d9ba780a6f1
Abhijindal905 <abhijindal905@gmail.com>
2025-07-07 14:54:35 +0530

    update wsgi.py

--- backend/chat/chat/wsgi.py
+++ backend/chat/chat/wsgi.py
@@ -6,11 +6,17 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
+import sys
 import os
 
+# Add your project directory to the sys.path
+project_path = '/home/abhijindal905/Real-Time-Chat-App/backend/chat'
+if project_path not in sys.path:
+    sys.path.insert(0, project_path)
+
+# Set the environment variable for Django settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'chat.settings'
+
+# Get the WSGI application
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
-
 application = get_wsgi_application()
