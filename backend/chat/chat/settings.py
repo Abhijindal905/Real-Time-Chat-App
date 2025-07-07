1b6a78e4d9d9174386936d028a88755e7537f2ef
Abhijindal905 <abhijindal905@gmail.com>
2025-07-07 14:56:55 +0530

    update setting.py

--- backend/chat/chat/settings.py
+++ backend/chat/chat/settings.py
@@ -107,3 +107,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEBUG = False
+ALLOWED_HOSTS = ['abhijindal905.pythonanywhere.com']
