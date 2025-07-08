0e25719c54ccad8e0cd0fc632996537bfd77a1da
Abhijindal905 <abhijindal905@gmail.com>
2025-07-08 15:43:06 +0530

    Update build.sh

--- backend/chat/build.sh
+++ backend/chat/build.sh
@@ -2,8 +2,8 @@ set -o errexit
 
 cd "$(dirname "$0")"
 
-pip install -r requirements.txt
+pip install -r ../requirements.txt
 
-python manage.py collectstatic --no-input
+python ../manage.py collectstatic --no-input
 
-python manage.py migrate
+python ../manage.py migrate
