0f39388e6a7c61b1e27b46a68fa6735c464c7a23
Abhijindal905 <abhijindal905@gmail.com>
2025-07-08 17:50:55 +0530

    Update build.sh

--- backend/chat/build.sh
+++ backend/chat/build.sh
@@ -6,6 +6,8 @@ pip install -r ../chat/requirements.txt
 
 python ../chat/manage.py collectstatic --no-input
 
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('Abhi123', 'admin@example.com', 'Abhi@123')" | python ../manage.py shell
+
 python ../chat/manage.py makemigrations
 
 python ../chat/manage.py migrate --no-input
