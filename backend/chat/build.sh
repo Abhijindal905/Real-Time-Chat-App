70f719b11e9b6803335ae67e4ec05faeedc2cb4b
Abhijindal905 <abhijindal905@gmail.com>
2025-07-09 12:17:21 +0530

    update build.sh

--- backend/chat/build.sh
+++ backend/chat/build.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -o errexit
 
 cd "$(dirname "$0")"
@@ -5,16 +6,16 @@ cd "$(dirname "$0")"
 pip install -r ../chat/requirements.txt
 
 python ../chat/manage.py collectstatic --no-input
+python ../chat/manage.py makemigrations
+python ../chat/manage.py migrate --no-input
 
-echo "from django.contrib.auth import get_user_model;
+# ✅ Create admin user
+echo "
+from django.contrib.auth import get_user_model;
 User = get_user_model();
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@example.com', 'adminpass');
     print('✅ Superuser created');
 else:
-    print('ℹ️ Superuser already exists');" | python manage.py shell
-
-
-python ../chat/manage.py makemigrations
-
-python ../chat/manage.py migrate --no-input
+    print('ℹ️ Superuser already exists');
+" | python ../chat/manage.py shell
