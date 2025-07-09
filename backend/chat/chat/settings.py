f06da5cacf014a70a189378edfd8e92cb722ce75
Abhijindal905 <abhijindal905@gmail.com>
2025-07-09 10:25:47 +0530

    Update settings.py

--- backend/chat/chat/settings.py
+++ backend/chat/chat/settings.py
@@ -96,12 +96,16 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://djangobackend_23rl_user:7ApOD42KJ8L7YncDuaxUROKY1ketQH7R@dpg-d1mb2fu3jp1c73eicplg-a.db.render.com/djangobackend_23rl',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 # Password validation
