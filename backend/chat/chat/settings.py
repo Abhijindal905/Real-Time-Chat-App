bc0bce54ba86c81eae2cd0745d3929097c73a9dc
Abhijindal905 <abhijindal905@gmail.com>
2025-07-10 17:33:06 +0530

    added aws policy

--- backend/chat/chat/settings.py
+++ backend/chat/chat/settings.py
@@ -167,6 +167,9 @@ AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "ap-south-1")
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 
+AWS_DEFAULT_ACL = 'public-read'
+
+
 AWS_QUERYSTRING_AUTH = False 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # for local fallback
