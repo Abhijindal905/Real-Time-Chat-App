f20765444ac67dbbde7d44410924c4261368caa9
Abhijindal905 <abhijindal905@gmail.com>
2025-07-10 17:50:41 +0530

    Update settings.py

--- backend/chat/chat/settings.py
+++ backend/chat/chat/settings.py
@@ -158,18 +158,16 @@ REST_FRAMEWORK = {
     )
 }
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DDEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "ap-south-1")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "eu-north-1")
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 
+AWS_QUERYSTRING_AUTH = False
 AWS_DEFAULT_ACL = 'public-read'
-
-
-AWS_QUERYSTRING_AUTH = False 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # for local fallback
