c872db95277cd9d08e4cf7187ac2fd24896b2946
Abhijindal905 <abhijindal905@gmail.com>
2025-07-11 00:07:25 +0530

    Update settings.py

--- backend/chat/chat/settings.py
+++ backend/chat/chat/settings.py
@@ -168,6 +168,5 @@ AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 
 AWS_QUERYSTRING_AUTH = False
-AWS_DEFAULT_ACL = 'public-read'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # for local fallback
