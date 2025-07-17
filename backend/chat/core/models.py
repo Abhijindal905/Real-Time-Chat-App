ad262c18bd40b8271917281516cf6f060a3797cb
Abhijindal905 <abhijindal905@gmail.com>
2025-07-17 11:27:21 +0530

    Update models.py

--- backend/chat/core/models.py
+++ backend/chat/core/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    profile_image = models.ImageField(upload_to='profile_images/', default='profile_images/default.png')
+    profile_image = models.ImageField(upload_to='profile_images/', default='profile_images/default.png', null=True, blank=True)
 
     def __str__(self):
         return self.user.username
