d90cbb07f36a254ba989ca37e8a58f5281c4286e
Abhijindal905 <abhijindal905@gmail.com>
2025-07-19 22:12:39 +0530

    Update FetchUser.jsx

--- frontend/chat/src/Helpers/FetchUser.jsx
+++ frontend/chat/src/Helpers/FetchUser.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export const fetchUsers = async (setUsers) => {
     try {
         const res = await axios.get(`${import.meta.env.VITE_API_URL}list_users/`);
+        console.log("fetching users")
         setUsers(res.data);
     } catch (error) {
         console.error("Error fetching users", error);
