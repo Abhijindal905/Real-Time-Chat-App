df4c3210566c9de09ccab4cb8bd388924ca16b21
Abhijindal905 <abhijindal905@gmail.com>
2025-07-19 16:47:59 +0530

    updated

--- backend/chat/chat/deployment_settings.py
+++ backend/chat/chat/deployment_settings.py
@@ -48,7 +48,7 @@ DATABASES = {
 
 # ✅ CORS settings
 CORS_ALLOWED_ORIGINS = [
-    "https://real-time-chat-app-frontend-eu0j.onrender.com",
+    "https://chat-app-61j5.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True 
